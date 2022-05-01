155fca91423e989608b63e12cb020c1d890dca85
ciuti <ciutigacenga@gmail.com>
2022-05-01 21:38:02 +0000

    edit: updated classification model

--- scripts/popular.py
+++ scripts/popular.py
@@ -12,24 +12,13 @@ from pyspark.sql.functions import regexp_extract
 
 
 class MostPopular(MRJob):
-#     re.compile(r"[\w']+")
-#     def isEnglish(text):
-#         try:
-#             if detect(text) != 'en':
-#                 return "No"
-#             return "Yes"
-#         except:
-#             return "No"
-#         return "No"
-# # Convert function to U
      
     def mapper(self,_,line ):
         #line=line.strip()
         #line=list(line.split(','))
         if len(list(line.split(",")))==22:
             (created_utc, ups, subreddit_id, link_id, name, score_hidden, author_flair_css_class, author_flair_text, subreddit, id, removal_reason, gilded, downs, archived, author, score, retrieved_on, body, distinguished, edited, controversiality, parent_id) = line.split(",")
-            #if isEnglish(body)=="Yes":
-            #if created_utc.isnumeric()==True:
+           
             yield subreddit,1
                 
     def reducer(self,key,value):
